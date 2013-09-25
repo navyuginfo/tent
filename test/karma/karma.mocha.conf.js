3fd72f599846f6d05c4d137cd459af47ccd1f233
Niall Fallon <niall.fallon@gmail.com>
2013-09-25 18:48:33 +0100

    Upgrade to karma 0.10.2 and latest grunt-karma.

--- test/karma/karma.mocha.conf.js
+++ test/karma/karma.mocha.conf.js
@@ -1,27 +1,33 @@
-// Karma configuration
-// Generated on Tue May 07 2013 16:59:16 GMT+0100 (IST)
+module.exports = function(config) {
+  config.set({
+    basePath: '',
+    frameworks: ['mocha'],
+    files: [
+      '../../test/karma/lib/chai.js',
+    '../../dist/javascript/jquery.js',
+    //'test/static/javascript/sinon-1.3.4.js',
+    //'test/static/javascript/sinon-qunit-1.0.0.js',
+    //'dist/javascript/vendor.js',
+    //'dist/javascript/compiled-templates.js',
+    //'dist/javascript/tent.js',
+    //'.tmp/test/lib/test_helpers.js',
+    '../../test/mocha/tests/test.js'
+    ],
+    // list of files to exclude
+    exclude: [],
+    reporters: ['dots'],
+    port: 9876,
+    runnerPort: 9100,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: false,
+    browsers: ['PhantomJS'],
+    captureTimeout: 60000,
+    singleRun: true
+  });
+};
 
-basePath = '';
-files = [
-  MOCHA,
-  MOCHA_ADAPTER,
-  '../../test/karma/lib/chai.js',
-  '../../dist/javascript/jquery.js',
-  //'test/static/javascript/sinon-1.3.4.js',
-  //'test/static/javascript/sinon-qunit-1.0.0.js',
-  //'dist/javascript/vendor.js',
-  //'dist/javascript/compiled-templates.js',
-  //'dist/javascript/tent.js',
-  //'.tmp/test/lib/test_helpers.js',
-  '../../test/mocha/tests/test.js'
-];
-exclude = [];
-reporters = ['dots'];
-port = 9876;
-runnerPort = 9100;
-colors = true;
-logLevel = LOG_INFO;
-autoWatch = false;
-browsers = ['PhantomJS'];
-captureTimeout = 60000;
-singleRun = true;
+  
+ 
+
+
