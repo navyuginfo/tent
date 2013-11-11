11e131deb12aa841836e15536207547e0dfd85da
Niall Fallon <niall.fallon@gmail.com>
2013-11-11 17:26:01 +0000

    Catch case for clicking in empty menu area.

--- dist/tent.js
+++ dist/tent.js
@@ -9729,42 +9729,6 @@ Tent.ModalPane = Ember.View.extend({
 
 
 
-/**
-* @class Tent.ProgressBar
-* 
-* Usage
-* 		{{view Tent.ProgressBar isStriped=true progress="50" isAnimated=true}}
-*/
-
-
-(function() {
-
-  Tent.ProgressBar = Ember.View.extend({
-    classNames: ['tent-progress-bar', 'progress'],
-    classNameBindings: ['isStriped:progress-striped', 'isAnimated:active'],
-    template: Ember.Handlebars.compile('<div class="bar" {{bindAttr style="view.style"}}></div>'),
-    /**
-    * @property {Boolean} isAnimated Boolean to indicate if the bar should be rendered with a progress animation.
-    */
-
-    isAnimated: false,
-    /**
-    * @property {Boolean} isStriped Boolean to indicate if the bar should be rendered with stripes
-    */
-
-    isStriped: false,
-    /**
-    * @property {Number} progress The progress to be displayed, as a percentage between 0 and 100
-    */
-
-    progress: 0,
-    style: Ember.computed(function() {
-      return "width:" + this.get('progress') + "%;";
-    }).property('progress')
-  });
-
-}).call(this);
-
 
 Ember.TEMPLATES['button']=Ember.Handlebars.compile("<div \t{{bindAttr class=\"view.classes view.buttonClass\"}}\n\t\t{{action triggerAction target=\"view\"}}\n\t\t{{bindAttr data-toggle=\"view.dataToggle\"}}\n    {{bindAttr disabled=\"view.isDisabled\"}}\n    {{bindAttr title=\"view.localizedTitle\"}}\n\t\trole=\"button\"\n\t\t>\n  <i {{bindAttr class=\"view.iconClass\"}}></i> {{view.localizedLabel}}\n  {{#if view.hasOptions}}\n  \t <span class=\"caret\"></span>\n  {{/if}}\n</div>\n{{#if view.hasOptions}}\n\t{{collection contentBinding=\"view._options\" tagName=\"ul\" classNames=\"dropdown-menu\" itemViewClass=\"Tent.ButtonOptions\"}}\n{{/if}}\n\n{{#if view.warn}}\n\n\t{{#view Tent.ModalPane \n          viewName=\"warningPanel\"\n          autoLaunch=false\n          header=\"tent.warning.header\" \n          primaryLabel=\"tent.button.proceed\" \n          secondaryLabel=\"tent.button.dontProceed\"\n          primaryType=\"warning\"\n          primaryIcon=\"icon-ok icon-white\"\n          secondaryIcon=\"icon-remove\"\n          primaryAction=\"ignoreWarnings\"\n          primaryTargetBinding=\"view\"\n    }}\n    \t{{loc tent.warning.warningsOnPage}}\n    \t \n  \t\t{{#each view.parentView.messagePanel.warning}}\n  \t\t\t\t<div class=\"alert\">\n  \t\t\t\t\t<strong>{{loc label}}:</strong>  {{messages}}\n  \t\t\t\t</div>\n  \t\t{{/each}}\n\t\t\t \n    {{/view}}\n{{/if}}\n\n{{#if view.shouldConfirm}}\n\n  {{#view Tent.ModalPane \n          viewName=\"confirmationPanel\"\n          autoLaunch=false\n          headerBinding=\"view.confirmationTitle\" \n          primaryLabelBinding=\"view.confirmationYes\" \n          secondaryLabelBinding=\"view.confirmationNo\"\n          primaryType=\"warning\"\n          primaryIcon=\"icon-ok icon-white\"\n          secondaryIcon=\"icon-remove\"\n          primaryAction=\"confirmAction\"\n          primaryTargetBinding=\"view\"\n          closeAction=\"view.confirmationNo\"\n    }}\n\n      {{loc view.parentView.confirmationMessage}}\n      \n       \n  {{/view}}\n{{/if}}");
 
@@ -12635,7 +12599,7 @@ Tent.Application.MenuItemView = Ember.View.extend({
         if (this.isLevelHeader(target)) {
           return;
         }
-        if (this.hasChildLevel(target) && !this.isDisabled(target)) {
+        if (this.hasChildLevel(target) && !this.isDisabled(target) && !target.is('ul')) {
           return this.navigateToNewLevel(e);
         } else {
           if (this.get('collapseAutomatically')) {
